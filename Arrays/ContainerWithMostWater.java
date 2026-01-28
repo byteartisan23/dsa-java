79f15d94c1dbc9446f812464be5970ed71a0380c
byteartisan23 <fahimgolder67@gmail.com>
2026-01-29 01:18:51 +0530

    Create ContainerWithMostWater.java
    container with most water using two pointer array approach and O(n) time complexity

---
+++ Arrays/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int maxArea(int[] height)
+    {
+        int left=0,right=height.length-1,max=0;
+        while(left<right)
+        {
+            int w=right-left;
+            int h=Math.min(height[left],height[right]);
+            int area=h*w;
+            max=Math.max(area,max);
+             if(height[left]<height[right])
+             {left++;}
+             else
+             {right--;}
+
+        }
+        return max;
+    }
+}
